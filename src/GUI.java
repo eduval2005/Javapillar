9e4634a633474d67d1770fca1fa579f0c3f7be32
JohnCarscallen <158017534+JohnCarscallen@users.noreply.github.com>
2024-02-20 18:02:51 -0500

    Add files via upload
    Needed panel.setVisible(false) when leaving the intro screen or bits of it would stay sometimes. Commented and re-organized.

--- src/GUI.java
+++ src/GUI.java
@@ -25,6 +25,67 @@ public class GUI {
     private JPanel panel;
     private BufferedImage backgroundImage;
 
+    //Default constructor
+    public GUI() {
+
+    //Initializing components (JButton, JLabel and JPanel)
+
+        playMusic("Intro.wav");
+
+        startButton = new JButton();
+            startButton.setText("Start");
+            startButton.setToolTipText("Hello there!");
+        //Sets the action listener of button to ButtonHandler (a class found later on)
+            startButton.addActionListener(new ButtonHandler());
+            startButton.setActionCommand("start");
+            startButton.setBounds(250,220,100,50);
+        title = new JLabel();
+            title.setText("Javapillar");
+            title.setFont(new Font("Arial", Font.PLAIN, 58));
+            title.setBounds(175,100,250,100);
+
+        panel = new splashPanel();
+        panel.setLayout(null);
+
+        gui = new JFrame();
+
+        //Sets the details of the JFrame
+        gui.setResizable(false);
+        gui.setSize(600,500);
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setVisible(true);
+        gui.setTitle("Javapillar");
+
+        //Adding previous components to panel
+        panel.add(startButton);
+        panel.add(title);
+        panel.setVisible(true);
+        panel.setBorder(BorderFactory.createLineBorder(Color.black));
+
+        //Adding panel to JFrame (since the class is extended from JFrame)
+        gui.add(panel);
+    }
+
+    //The button actions are below
+    private class ButtonHandler implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+
+            //The action when you press the start button
+            if (e.getActionCommand().equals("start")) {
+                panel.setVisible(false); //otherwise it sometimes leaves bits on the screen
+                clip.stop();             //stop the intro music or it just keeps playing
+                playMusic("Game.wav");
+                gui.add(new Controller());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+    }
+
+    //To put a background we have to override the
+    //JPanel class paintComponent method
     public class splashPanel extends JPanel{
         private BufferedImage backgroundImage;
         public splashPanel(){
@@ -38,17 +99,18 @@ public class GUI {
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
-            if (backgroundImage != null) {
-                g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
-            }
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
         }
     }
 
+    //A method to play WAV files for music
+    //stop the music with clip.stop()
     public void playMusic(String filePath){
         try {
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
             clip = AudioSystem.getClip();
             clip.open(audioInputStream);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
             clip.start();
         }
 
@@ -57,72 +119,4 @@ public class GUI {
             ex.printStackTrace();
         }
     }
-
-//Default constructor
-public GUI() {
-
-//Initializing components (JButton, JLabel and JPanel)
-
-    playMusic("Intro.wav");
-
-    startButton = new JButton();
-        startButton.setText("Start");
-        startButton.setToolTipText("Hello there!");
-       //Sets the action listener of button to ButtonHandler (a class found later on)
-        startButton.addActionListener(new ButtonHandler());
-        startButton.setActionCommand("start");
-        startButton.setBounds(250,220,100,50);
-    title = new JLabel();
-        title.setText("Javapillar");
-        title.setFont(new Font("Arial", Font.PLAIN, 58));
-        title.setBounds(175,100,250,100);
-    panel = new splashPanel();
-        panel.setLayout(null);
-        //JLabel background = new JLabel(new ImageIcon("Caterpillar_splash.png"));
-        //background.setBounds(0, 0, 600, 500);
-        //background.setOpaque(false);
-
-    gui = new JFrame();
-
-    //Sets the details of the JFrame
-    gui.setResizable(false);
-    gui.setSize(600,500);
-    gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    gui.setVisible(true);
-    gui.setTitle("Javapillar");
-
-
-//Adding previous components to panel
-
-    panel.add(startButton);
-    panel.add(title);
-    panel.setVisible(true);
-    panel.setBorder(BorderFactory.createLineBorder(Color.black));
-
-//Adding panel to JFrame (since the class is extended from JFrame)
-
-    gui.add(panel);
-}
-
-//The button actions are below
-
-private class ButtonHandler implements ActionListener {
-    public void actionPerformed(ActionEvent e) {
-
-        //The action when you press the start button
-        if (e.getActionCommand().equals("start")) {
-            title.setVisible(false);
-            startButton.setVisible(false);
-            clip.stop();
-            playMusic("Game.wav");
-            gui.add(new Controller());
-        }
-    }
-}
-
-public static void main(String[] args) {
-
-    new GUI();
-    
-}
 }
